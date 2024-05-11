594c1a6fe9ca1fc192ca4c317260a614d670545b
Anujkulal <anujkulal333@gmail.com>
2024-05-11 17:09:24 +0530

    initial commit

--- 1_script.js
+++ 1_script.js
@@ -6,4 +6,14 @@
 // const area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
 // console.log(`The area of the traingle is ${area}`);
 
-// // 2). JS progra
+// // 2). JS program to swap 2 numbers using destructuring assignment
+// let a = prompt("Enter the first number: ");
+// let b = prompt("Enter the second number: ");
+// console.log(`Before swaping: a = ${a}, b = ${b}`);
+// [a, b] = [b, a];
+// or 
+// //a = a + b;
+// //b = a - b;
+// //a = a - b;
+// console.log(`After swaping: a = ${a}, b = ${b}`);
+

75e799105e98e377048d02feb810ad3c214a9745
Anujkulal <anujkulal333@gmail.com>
2024-05-11 17:05:51 +0530

    initial commit

---
+++ 1_script.js
@@ -0,0 +1,9 @@
+// // 1). JS program to find the area of a triangle
+// const side1 = parseInt(prompt("Enter side 1: "));
+// const side2 = parseInt(prompt("Enter side 2: "));
+// const side3 = parseInt(prompt("Enter side 3: "));
+// const semiPerimeter = (side1 + side2 + side3)/2;
+// const area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+// console.log(`The area of the traingle is ${area}`);
+
+// // 2). JS progra

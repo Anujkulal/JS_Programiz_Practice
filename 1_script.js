b56dca3a4fc271a9709eed9c84add4a0b95677d1
anujkulal <anujkulal333@gmail.com>
2024-06-09 22:29:28 +0530

    commit

--- 1_script.js
+++ 1_script.js
@@ -6,14 +6,17 @@
 // const area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
 // console.log(`The area of the traingle is ${area}`);
 
+
 // // 2). JS program to swap 2 numbers using destructuring assignment
 // let a = prompt("Enter the first number: ");
 // let b = prompt("Enter the second number: ");
 // console.log(`Before swaping: a = ${a}, b = ${b}`);
 // [a, b] = [b, a];
-// or 
+// //or 
 // //a = a + b;
 // //b = a - b;
 // //a = a - b;
 // console.log(`After swaping: a = ${a}, b = ${b}`);
 
+
+// //3). JS program to git add README.md
